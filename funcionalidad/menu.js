19f2d85057807ecafacb73307be70f061dbc7468
Evanny <hbpgvanny@gmail.com>
2022-06-06 18:06:36 -0500

    solucion completa

--- funcionalidad/menu.js
+++ funcionalidad/menu.js
@@ -6,11 +6,6 @@ const $bars_icon = d.querySelector(".ham-icon");
 const $close_icon = d.querySelector(".close-icon");
 const $menu = d.querySelector(".nav-menu");
 
-console.log($ham_button);
-console.log($bars_icon);
-console.log($close_icon);
-console.log($menu);
-
 $ham_button.addEventListener("click", () => {
   $bars_icon.classList.toggle("clicked");
   $close_icon.classList.toggle("clicked");
